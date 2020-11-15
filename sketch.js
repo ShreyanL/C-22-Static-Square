da4a6c7e3f6e0de038dd87699cd508e4012d301c
Shreyan Lowalekar <shreyan.lowalekar@gmail.com>
2020-11-15 18:16:13 +0530

    Renamed to myEngine & myWorld variables and added comments

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ var ball;
 
 function setup()
 {
-    var canvas = createCanvas(400,400);
+    var canvas = createCanvas(400, 400);
     myEngine = Engine.create();
     myWorld = myEngine.world;
 
@@ -21,6 +21,8 @@ function setup()
     World.add(myWorld, ground);
 
     console.log(ground);
+
+    //Make ball bounce to max. using restitution
     var options =  { restitution:1.0 }
     ball = Bodies.circle(200, 100, 20, options);
     World.add(myWorld, ball)
@@ -31,8 +33,12 @@ function draw()
    
     background(0);
     Engine.update(myEngine);
+
+    //Show the ground
     rectMode(CENTER);
     rect(ground.position.x, ground.position.y, 400, 20);
+    
+    //Show the ball
     ellipseMode(RADIUS);
     ellipse(ball.position.x, ball.position.y, 20, 20);
     
